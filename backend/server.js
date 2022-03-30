001feadafd25fdfc2188a13045557e6b3e4738aa
ouko <oukoezzy@gmail.com>
2022-03-30 20:14:49 +0300

    rest API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMidlleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
-
+connectDB()
 const app = express()  //initialize express
 
 app.use(express.json())
