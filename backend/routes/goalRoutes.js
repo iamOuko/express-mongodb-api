21d2dc46f1198180e6d08f4e1267a31e3a87586c
ouko <oukoezzy@gmail.com>
2022-03-31 16:46:16 +0300

    added authentication

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,14 +5,15 @@ const {getGoals,
         updateGoal,
         deleteGoal,
     } = require('../controllers/goalController')
+const { protect } = require('../middleware/authMiddleware')
 
 // router.get('/', getGoals)
 // router.post('/', setGoal)
-router.route('/').get(getGoals).post(setGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
 
 // router.put('/:id', updateGoal)
 // router.delete('/:id', deleteGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 
 module.exports = router
